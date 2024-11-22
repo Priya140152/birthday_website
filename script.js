769df62d4f5c560c687ddfc854e706c016f8423b
Priya140152 <ppriyadharshini289@gmail.com>
2024-11-22 17:15:05 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+// Function to toggle between login and signup forms
+function toggleForm() {
+    const loginForm = document.getElementById('login-form');
+    const signupForm = document.getElementById('signup-form');
+    
+    // Toggle visibility
+    loginForm.classList.toggle('hidden');
+    signupForm.classList.toggle('hidden');
+  }
+  
